ecf8013bfdfd30c0049f236f37d64097c733ee82
João Victor <joaovictordsbrito@gmail.com>
2020-03-27 15:53:44 -0300

    Implementação Banco de Dados

--- index.js
+++ index.js
@@ -5,8 +5,31 @@
  */
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+var mysql = require('mysql');
 
 var bot = new Discord.Client();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "dbgearscore"
+});
+
+con.connect(function (error) {
+    if (!!error) {
+        console.log('Erro')
+    } else {
+        console.log('Conectado')
+    }
+})
+
+/*con.connect(function (err) {
+    con.query("SELECT * FROM gearscore", function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });*/
 
 /**
  * Inicia o Bot e deixa em "Ready" como um listener e mostra no log que está funcionando.
@@ -21,14 +44,30 @@ bot.on("ready", function () {
  * Funcionamento: O Bot vai escutar uma mensagem, se for relacionada a um comando programado ele vai dividir a mensagem e salvar em cada posição do vetor e depois mostrar a mensagem.
  */
 bot.on("message", function (message) {
-    if (message.author.equals(bot.user)) return; //Retorna pra fora da função caso a mensagem seja do proprio bot.
+    if (message.author.equals(bot.user)) return;
+    const gs = message.content.slice(prefix.lenght).split(' ');
+
     if (message.content.startsWith(`${prefix}addgs`) && gs[1].startsWith('<@')) {
-            const gs = message.content.slice(prefix.lenght).split(' '); //Pega o conteudo da mensagem do usuario e vai dividir em virgulas e salvar dentro de um vetor. Precisa por dentro das condições a baixo.
-            message.channel.send(`GS e Classe ${gs[1]} ${gs[2]} ${gs[3]} ${gs[4]} ${gs[5]} ${gs[6]}`); //O Discord reconhece menções como @Fulano com base a id dele, é utlizada uma tag especial para mencionar via codigo; Ex @Fulano = <@123456>
+        con.query('INSERT INTO gearscore values(' + message.mentions.users.first().id + ",'" + gs[2] + "'," + gs[3] + ', ' + gs[4] + ', ' + gs[5] + ', ' + gs[6] + ')');
+        message.channel.send(`GS e Classe ${gs[1]} ${gs[2]} ${gs[3]} ${gs[4]} ${gs[5]} ${gs[6]}`);
     }
-    if(message.content.startsWith(`${prefix}addgs`) && !gs[1].startsWith('<@')){
-        const gs = message.content.slice(prefix.lenght).split(' ');
-        message.channel.send(`GS e Classe <@` + message.author + `> ${gs[1]} ${gs[2]} ${gs[3]} ${gs[4]}`) //Mostrando a mensagem que foi dividia em partes com espaços entre.
+    if (message.content.startsWith(`${prefix}addgs`) && !gs[1].startsWith('<@')) {
+        con.query('INSERT INTO gearscore values(' + message.author + ",'" + gs[1] + "'," + gs[2] + ', ' + gs[3] + ', ' + gs[4] + ', ' + gs[5] + ')');
+        message.channel.send(`GS e Classe <@` + message.author + `> ${gs[1]} ${gs[2]} ${gs[3]} ${gs[4]}`)
+    }
+
+    if (message.content.startsWith(`${prefix}gs`) && gs[1].startsWith('<@')) {
+        con.query('SELECT id, classe, nivel, ap, apw, dp from gearscore where id = ' + message.mentions.users.first().id, function (err, gs) {
+            if (err) throw err;
+            message.channel.send('Seu GS <@' + gs[0].id + '> ' + gs[0].classe + ' ' + gs[0].nivel + ' ' + gs[0].ap + ' ' + gs[0].apw + ' ' + gs[0].dp);
+        })
+    }
+    if (message.content.startsWith(`${prefix}gs`) && !gs[1].startsWith('<@')) {
+        con.query('SELECT id, classe, nivel, ap, apw, dp from gearscore where id = ' + message.author, function (err, gs) {
+            if (err) throw err;
+            message.channel.send('Seu GS <@' + gs[0].id + '> ' + gs[0].classe + ' ' + gs[0].nivel + ' ' + gs[0].ap + ' ' + gs[0].apw + ' ' + gs[0].dp);
+        })
+
     }
 });
 
