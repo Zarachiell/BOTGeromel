7e41e153d042c4c97fd829f2e4e308470ace3148
joao.victor <joao.victor@esig.com.br>
2021-05-16 21:36:49 -0300

    Implementação da busca do youtube no bot

--- index.js
+++ index.js
@@ -4,18 +4,20 @@
  */
 const Discord = require('discord.js');
 const { prefix, prefix2, token } = require('./config.json');
-var mysql = require('mysql');
-var db = require('./connectiondb');
 var bot = new Discord.Client();
 const YouTube = require("discord-youtube-api");
 const youtube = new YouTube("google api key");
+const ytdl = require('ytdl-core');
+const yts = require("yt-search");
+var { google } = require("googleapis");
+var youtubeV3 = google.youtube({ version: 'v3', auth: 'AIzaSyCyYXonS1F2g50nBXiPlBZXSZB9xLT_X2o' });
 
 /**
  * Inicia o Bot e deixa em "Ready" como um listener e mostra no log que está funcionando.
  */
 
 bot.on("ready", function () {
-    console.log("A Funcionar");
+  console.log("A Funcionar");
 });
 
 /**
@@ -23,41 +25,78 @@ bot.on("ready", function () {
  * Funcionamento: O Bot vai escutar uma mensagem, se for relacionada a um comando programado ele vai dividir a mensagem e salvar em cada posição do vetor e depois mostrar a mensagem.
  */
 bot.on("message", function (message) {
-    if (message.author.equals(bot.user)) return;
-    const mensagem = message.content;
-    execucaoComandos(mensagem);
-
+  if (message.author.equals(bot.user)) return;
+  const mensagem = message;
+  execucaoComandos(mensagem);
 });
 
 
-function execucaoComandos(mensagem) {
-    if (mensagem.startsWith(`${prefix}brackets`)) message.channel.send("AP/DP Brackets", { files: ["https://cdn.discordapp.com/attachments/278999893903802369/694004818326323260/unknown.png"] });
-    if (mensagem.startsWith(`${prefix}comandos`)) message.channel.send('Lista de Comandos Disponíveis: !addgs; !gs; !brackets; !comandos');
-    if (mensagem.startsWith(`${prefix}DALE`)) message.channel.send('DALE', { tts: true });
-    if (mensagem.startsWith(`${prefix2}`)) message.channel.send(message.content.split("*"), { tts: true });
-    if (mensagem.startsWith(`${prefix}play`)) iniciarBusca(mensagem);
+function execucaoComandos(messagem) {
+  if (messagem.content.startsWith(`${prefix}brackets`)) messagem.channel.send("AP/DP Brackets", { files: ["https://cdn.discordapp.com/attachments/278999893903802369/694004818326323260/unknown.png"] });
+  if (messagem.content.startsWith(`${prefix}comandos`)) messagem.channel.send('Lista de Comandos Disponíveis: !addgs; !gs; !brackets; !comandos');
+  if (messagem.content.startsWith(`${prefix}DALE`)) messagem.channel.send('DALE', { tts: true });
+  if (messagem.content.startsWith(`${prefix2}`)) messagem.channel.send(messagem.content.split("*"), { tts: true });
+  if (messagem.content.startsWith(`${prefix}play`)) iniciarBusca(messagem);
+  if (messagem.content.startsWith(`${prefix}teste`)) teste(messagem);
 }
-function iniciarBusca(mensagem) {
-    const { voiceChannel } = mensagem.member;
 
-      if (!voiceChannel) {
-        return mensagem.reply('please join a voice channel first!');
-      }
+async function iniciarBusca(mensagem) {
+  const voiceChannel = mensagem.member.voice.channel;
+  const comando = `${prefix}play `;
+  const mensagemCortada = mensagem.content.slice(comando.length).split(' ');
 
-      voiceChannel.join().then(connection => {
-        const stream = ytdl(searchYouTubeAsync(args), { filter: 'audioonly' });
-        const dispatcher = connection.playStream(stream);
+  if (!isEmUmCanalDeVoz(mensagem)) {
+    return mensagem.reply('please join a voice channel first!');
+  }
+  voiceChannel.join().then(async connection => {
+    let url = searchYouTubeAsync(mensagemCortada.join(' '));
+    //let url2 = search(mensagemCortada.join(' '));
+    console.log(url);
+    let stream = ytdl(url, { filter: 'audioonly' });
+    let dispatcher = connection.play(stream);
+    console.log('teste');
+    dispatcher.on('end', () => voiceChannel.leave());
+    isReady = true;
+  }).catch(e => { console.log(e) })
+}
 
-        dispatcher.on('end', () => voiceChannel.leave());
-        isReady = true;
-      })
+function isEmUmCanalDeVoz(mensagem) {
+  const teste = mensagem.member.voice;
+  //console.log(teste);
+  return mensagem.member.voice.channelID != null
+}
+
+function teste(mensagem) {
+  const comando = '!teste ';
+  const mensagemCortada = mensagem.content.slice(comando.length).split(' ');
+  const mensagemConcatenada = mensagemCortada.join(' ');
+  console.log(mensagemConcatenada);
 }
 
 async function searchYouTubeAsync(args) {
-  var video = await youtube.searchVideos(args.toString().replace(/,/g,' '));
-  console.log(video.url);
-  console.log(typeof String(video.url));
-  return String(video.url);
+  youtubeV3.search.list({
+    part: 'snippet',
+    type: 'video',
+    q: args,
+    maxResults: 1,
+  }, (err, response) => {
+    const videoUrl = 'https://www.youtube.com/watch?v=' + response.data.items[0].id.videoId;
+    console.log(videoUrl);
+    return videoUrl
+  });
+}
+
+function search(args) {
+  var q = args;
+  var request = youtubeV3.search.list({
+    q: 'cat',
+    part: 'snippet',
+    type: 'video'
+  });
+  for (var i in request.items) {
+    var item = request.data.items[i].id.videoId;
+    console.log(item);
+  }
 }
 
 bot.login(token);
