3b9a51c57c29f2d324ffdeb8f2f01547adb2f935
Zarachiell <49499677+Zarachiell@users.noreply.github.com>
2021-09-18 15:19:03 -0300

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    prefix: "!",
-    prefix2: "*",
-    botKey: "NjkxNzMyMDAxNzYxMzk0Nzg4.XnkPoA.fTtbp2S072-2VOV4Cf_X1XQ3xvo",
-    googleAPIKey: "AIzaSyArdmI9oq-lyXTR_vlQ_5cshTAadN1aj0M"
-};
-
-module.exports = config;
