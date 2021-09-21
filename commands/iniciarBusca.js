05eb35d2368de227719972b737b20dde911b729a
joao.victor <joao.victor@esig.com.br>
2021-09-21 17:04:57 -0300

    Correção em merge

--- commands/iniciarBusca.js
+++ commands/iniciarBusca.js
@@ -1,4 +1,4 @@
-var config = require('../config/dev');
+var config = require('../config/config');
 const ytdl = require('ytdl-core');
 var { google } = require("googleapis");
 var youtubeV3 = google.youtube({ version: 'v3', auth: config.googleAPIKey });
